ee142702e5dcab7a4dea369fbe2aae57f556097a
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-03-19 23:24:41 +0300

    .

---
+++ RefOutParams/MathEx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace RefOutParams
+{
+    static class MathEx
+    {
+        public const double Fi = 1.618; //(a+b)/a = a / b;
+        public const double S = 2.414; //((2*a)+b)/a = a / b;
+        public const double K = 0.916;
+
+        public static bool Divide(double firstNumber, double secondNumber, out double result)
+        {
+            result = firstNumber / secondNumber;
+            if (secondNumber == 0 || firstNumber == 0)
+            {
+                return false;
+            }
+            return true;
+            
+
+        }
+        public static bool Pow(ref double value)
+        {
+            bool result;
+            if (value >= 0)
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+            value = Math.Pow(value, 2);
+            return result;
+        }
+
+        public static double Sum(params double[] numbers)
+        {
+            double result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+            return result;
+        }
+    }
+}
--- RefOutParams/Program.cs
+++ RefOutParams/Program.cs
@@ -10,6 +10,18 @@ namespace RefOutParams
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Золотое сечение = {MathEx.Fi}\nCеребряное сечение ={MathEx.S}\nПостоянная Каталана = {MathEx.K}" );
+            double result;
+
+            double first = -5;
+            double second = 10;
+
+            Console.WriteLine(MathEx.Divide(first, second, out result));
+            Console.WriteLine(MathEx.Pow(ref first));
+            Console.WriteLine(first);
+            Console.WriteLine(MathEx.Sum(5, 6, 7, 8));
+
+            Console.ReadKey();
         }
     }
 }
